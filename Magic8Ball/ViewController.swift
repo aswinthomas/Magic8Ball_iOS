f4d677ff53e954a8581975986d5df8ecec3e0ad5
aswinthomas <aswinthomas@gmail.com>
2024-01-03 19:20:51 +0800

    add ss

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        let imageStr = "ball\(Int.random(in:1...5))"
+        print("Button pressed. ImageStr=\(imageStr)")
+        
+        imageView.image = UIImage(named: imageStr)
     }
-
-
+    
 }
 
